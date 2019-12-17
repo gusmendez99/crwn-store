9cc9ad70fdf7a6438695dcb384b600a9a4f13b61
Gus Mendez <gus.mendez.99@gmail.com>
2019-12-17 16:55:51 -0600

    created project and added files for homepage

---
+++ src/components/homepage/homepage.component.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import './homepage.styles.scss'
+
+export const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="subtitle">HATS</h1>
+        </div>
+			</div>
+			
+			<div className="menu-item">
+        <div className="content">
+          <h1 className="subtitle">HATS</h1>
+        </div>
+			</div>
+			
+			<div className="menu-item">
+        <div className="content">
+          <h1 className="subtitle">HATS</h1>
+        </div>
+			</div>
+			
+			<div className="menu-item">
+        <div className="content">
+          <h1 className="subtitle">HATS</h1>
+        </div>
+			</div>
+			
+			<div className="menu-item">
+        <div className="content">
+          <h1 className="subtitle">HATS</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+);
