47a4fb5aa986135c596fea8b05f005347c77beb2
Gus Mendez <gus.mendez.99@gmail.com>
2020-01-02 13:41:42 -0600

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
 
-import './cart-item.styles.scss'
+import React from 'react';
 
-const CartItem =({ item: { imageUrl, price, name, quantity } }) => (
-    <div className='cart-item'>
-        <img src={imageUrl} alt='item'/>
-        <div className='item-details'>
-            <span className='name'>{name}</span>
-            <span className='price'>{quantity} x ${price}</span>
-        </div>
+import './cart-item.styles.scss';
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+  <div className='cart-item'>
+    <img src={imageUrl} alt='item' />
+    <div className='item-details'>
+      <span className='name'>{name}</span>
+      <span className='price'>
+        {quantity} x ${price}
+      </span>
     </div>
-)
+  </div>
+);
 
-export default CartItem
+export default CartItem;
