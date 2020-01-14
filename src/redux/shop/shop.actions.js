0e2296494e964b893a3ac36578251372e0996a07
Gus Mendez <gus.mendez.99@gmail.com>
2020-01-14 12:03:57 -0600

    creating a redux-saga for fetching our collections from the firestore database

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,9 +1,5 @@
 import ShopActionTypes from "./shop.types";
 
-import {
-  firestore,
-  convertCollectionsSnapshotToMap
-} from "../../firebase/firebase.utils";
 
 export const fetchCollectionsStart = () => ({
   type: ShopActionTypes.FETCH_COLLECTION_START
@@ -21,14 +17,6 @@ export const fetchCollectionsFailure = errorMessage => ({
 
 export const fetchCollectionsStartAsync = () => {
   return dispatch => {
-    const collectionRef = firestore.collection("collections");
-    dispatch(fetchCollectionsStart())
-
-    collectionRef.get().then(snapshot => {
-      //console.log(snapshot)
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        dispatch(fetchCollectionsSuccess(collectionsMap))
-      this.setState({ loading: false });
-    }).catch(error => dispatch(fetchCollectionsFailure(error.message)));
+    
   };
 };
---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,38 @@
+import { takeLatest, call, put } from "redux-saga/effects";
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from "../../firebase/firebase.utils";
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure
+} from "./shop.actions";
+
+import ShopActionTypes from "./shop.types";
+
+function* fetchCollectionAsync() {
+  yield console.log("I am fired");
+
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+		);
+		
+		yield put(fetchCollectionsSuccess(collectionsMap))
+  } catch (error) {
+		yield put(fetchCollectionsFailure(error.message))
+	}
+}
+
+export function* fetchCollectionStart() {
+  // Take/TakeEvery/TakeLatest are an non-blocking calls
+  yield takeLatest(
+    ShopActionTypes.FETCH_COLLECTION_START, 
+    fetchCollectionAsync
+  );
+}
