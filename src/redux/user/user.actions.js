cb5bf7321215903be8e0b17353b909869217031e
Gus Mendez <gus.mendez.99@gmail.com>
2020-03-29 23:26:04 -0600

    react-scripts updated & fixed sign-in/sign-out saga

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -21,3 +21,16 @@ export const emailSignInStart = emailAndPassword => ({
   type: UserActionTypes.EMAIL_SIGN_IN_START,
   payload: emailAndPassword
 });
+
+export const signOutStart = () => ({
+  type: UserActionTypes.SIGN_OUT_START
+});
+
+export const signOutSuccess = () => ({
+  type: UserActionTypes.SIGN_OUT_SUCCESS
+});
+
+export const signOutFailure = error => ({
+  type: UserActionTypes.SIGN_OUT_FAILURE,
+  payload: error
+});
