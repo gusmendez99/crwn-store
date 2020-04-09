0625bbaf48ff90f35b44f03cae4e934e765802c5
Gus Mendez <gus.mendez.99@gmail.com>
2020-04-09 16:22:58 -0600

    updating our code to be a pwa compliant, added serviceworker, https, updated manifest

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ app.use(cors());
  
 if (process.env.NODE_ENV === 'production') {
   app.use(compression);
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
-  console.log('This config is working...')
+  //app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
  
   app.get('*', function(req, res) {
